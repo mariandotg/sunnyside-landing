82f988af04940f82b567be9217b92152a507fc4c
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-13 08:51:42 -0300

    Added tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,46 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    fontSize: {
+      DEFAULT: ['1.125rem', '1.5rem'],
+      title: ['2.5rem', '2.75rem'],
+    },
+    screens: {
+      mobile: '375px',
+      tablet: '834px',
+      desktop: '1024px',
+    },
+    fontFamily: {
+      fraunces: ['Fraunces', 'system-ui', 'serif'],
+      barlow: ['Barlow', 'system-ui', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      'soft-red': '#FE7867',
+      yellow: '#FAD400',
+      'dark-desaturated-cyan': '#25564B',
+      'dark-blue': '#19536B',
+      'dark-moderate-cyan': '#458C7E',
+      'very-dark-desaturated-blue': '#23303E',
+      'very-dark-grayish-blue': '#5A636C',
+      'dark-grayish-blue': '#818498',
+      'grayish-blue': '#A7ABAE',
+      white: '#FFFFFF',
+      'light-blue': '#3DBEFD',
+    },
+    animation: {
+      pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+    },
+    keyframes: {
+      pulse: {
+        '0%, 100%': {
+          opacity: 1,
+        },
+        '50%': {
+          opacity: 0.4,
+        },
       },
     },
   },
   plugins: [],
-}
+};
