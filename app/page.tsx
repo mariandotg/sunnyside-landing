145cd8bd6ccbeb2adcac3e8f73ee0b26e2f446fd
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-13 09:03:45 -0300

    Refactored hero into a section

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,22 @@ import IconArrowDown from '@/public/icon-arrow-down.svg';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between min-h-screen'>
-      <div className='h-[150px] w-full bg-light-blue'></div>
-      <div className='relative w-full flex justify-center items-center h-[400px]'>
-        <div className='absolute flex flex-col items-center justify-center w-full top-6 gap-y-12'>
-          <h1 className='z-10 font-black tracking-[.15em] text-center text-white uppercase font-fraunces text-title '>
-            We are creatives
-          </h1>
-          <IconArrowDown className='z-10 w-[36px] h-[116px]' />
+      <section className='bg-light-blue pt-[150px] w-full'>
+        <div className='relative w-full flex justify-center items-center h-[400px]'>
+          <div className='absolute flex flex-col items-center justify-center w-full top-6 gap-y-12'>
+            <h1 className='z-10 font-black tracking-[.15em] text-center text-white uppercase font-fraunces text-title '>
+              We are creatives
+            </h1>
+            <IconArrowDown className='z-10 w-[36px] h-[116px]' />
+          </div>
+          <Image
+            alt='Hero'
+            src='/desktop/image-header.jpg'
+            className='absolute z-0 object-cover -top-1'
+            fill={true}
+          />
         </div>
-        <Image
-          alt='Hero'
-          src='/desktop/image-header.jpg'
-          className='absolute z-0 object-cover -top-1'
-          fill={true}
-        />
-      </div>
+      </section>
     </main>
   );
 }
